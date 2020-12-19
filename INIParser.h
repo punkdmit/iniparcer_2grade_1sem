10f1fac1497335d777a2fcbe99d20ea91a69c26d
Dmitry <dima.gg2001@mail.ru>
2020-12-19 22:52:50 +0300

    Lab1 2 курс 1 сем

---
+++ INIParser.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <fstream>
+#include <string>
+#include <regex>
+#include <iostream>
+#include <cctype>
+#include <string.h>
+#include "ParsedINIFile.h"
+#include "INIFileBuilder.h"
+#include "MyException.h"
+
+class INIParser
+{
+public:
+    ParsedINIFile Parse(const char **filePath);
+};
